2194d100df141d40372bee71db47971563211e24
prodhc <zionywood@gmail.com>
2021-03-19 11:32:06 -0700

    Finished all main JavaScript Tasks

--- index.js
+++ index.js
@@ -293,11 +293,19 @@ Use addArtist to do the following:
 
 Example: addArtist(artists) should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/){
-    /*Your Code Here*/
+function addArtist(array){
+    array.push({
+      "id": 20,
+      "name": "Secoya Wood", 
+      "years": "2001 - 2021",
+      "genre": "Web Design", 
+      "nationality": "American",
+      "bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    })
+    return array;
   }
+  console.log(addArtist(artists));
 
-  
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Use lotsOfArt to do the following: 
@@ -306,8 +314,14 @@ Use lotsOfArt to do the following:
 
 For example lotsOfArt(artists); will return ["Amedeo Modigliani", "Rene Magritte", ... "Albrecht Dürer"]*/
 
-function lotsOfArt(/*Your Code Here*/){
-  /*Your Code Here*/
+function lotsOfArt(array){
+  let artArray = [];
+  for(let i = 0; i < array.length; i++) {
+    if(array[i]['paintings'] > 100) {
+      artArray.push(array[i]['name']);
+    }
+  }
+  return artArray;
 }
 
 
